a35846e43744fc03217bf087c7f26ba55ebf478a
Anjalireddy888 <saumiinturi@gmail.com>
2025-01-02 22:54:55 +0530

    jan2

---
+++ jan2.py
@@ -0,0 +1,18 @@
+# 2559. Count Vowel Strings in Ranges
+
+class Solution:
+    def vowelStrings(self, words, queries): 
+        n = len(words)
+        Prefix = [0] * (n + 1)
+        vowels = {'a', 'e', 'i', 'o', 'u'}
+
+        for i in range(n):
+            Prefix[i + 1] = Prefix[i]
+            if words[i][0] in vowels and words[i][-1] in vowels:
+                Prefix[i + 1] += 1
+
+        ANS = []
+        for query in queries:
+            ANS.append(Prefix[query[1] + 1] - Prefix[query[0]])
+
+        return ANS
