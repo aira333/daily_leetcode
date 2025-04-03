afdb130b37effb4c949cf5b678723ce82b1ab870
Anjalireddy888 <saumiinturi@gmail.com>
2025-04-03 23:12:18 +0530

    maximum problem of an ordered triplet II

---
+++ daily_leetcode/apr3.py
@@ -0,0 +1,16 @@
+# Maximum Value of an Ordered Triplet II
+
+class Solution:
+    def maximumTripletValue(self, nums: List[int]) -> int:
+        maxi = float('-inf')
+        diff = 0
+        res = 0
+        
+        for i in range(len(nums)):
+            maxi = max(maxi, nums[i])
+            if i >= 2:
+                res = max(res, diff * nums[i])
+            if i >= 1:
+                diff = max(diff, maxi - nums[i])
+                
+        return res
